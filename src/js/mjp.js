5f75fe1d82c28b8dc89c0e399be142fa84580ef6
mjp <1685372168@qq.com>
2017-09-21 14:51:01 +0800

    添加src文件夹

---
+++ src/js/mjp.js
@@ -0,0 +1,12 @@
+/* 
+* @Author: Marte
+* @Date:   2017-09-20 20:04:50
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-09-20 20:09:47
+*/
+
+window.onload=function(){
+
+    function show(){console.log(33)};
+    show();
+}
